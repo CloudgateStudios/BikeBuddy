02ad14e08c39b78e8646f7c3edfd5f10e0723c64
Tom Arra <tarra3@gmail.com>
2015-06-04 20:37:07 -0500

    Code cleanup

--- Bike Buddy/StationsListTableViewController.swift
+++ Bike Buddy/StationsListTableViewController.swift
@@ -24,14 +24,10 @@ class StationsListTableViewController: UITableViewController, CLLocationManagerD
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "refreshStationsData", name: NOTIFICATION_CENTER_FIRST_TIME_USE_COMPLETED, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "refreshStationsData", name: NOTIFICATION_CENTER_NEW_CITY_SELECTED, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "setupUI", name: NOTIFICATION_CENTER_STATIONS_LIST_UPDATED, object: nil)
     }
     
     deinit {
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: NOTIFICATION_CENTER_FIRST_TIME_USE_COMPLETED, object: nil)
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: NOTIFICATION_CENTER_NEW_CITY_SELECTED, object: nil)
         NSNotificationCenter.defaultCenter().removeObserver(self, name: NOTIFICATION_CENTER_STATIONS_LIST_UPDATED, object: nil)
     }
 
@@ -61,9 +57,9 @@ class StationsListTableViewController: UITableViewController, CLLocationManagerD
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(STATIONS_LIST_TABLE_CELL_REUSE_IDENTIFIER, forIndexPath: indexPath) as! StationTableViewCell
         
-        cell.stationNameLabel.text = Stations.sharedInstance.list[indexPath.row].stationName
-        cell.numberOfBikesLabel.text = String(Stations.sharedInstance.list[indexPath.row].availableBikes)
-        cell.numberOfAvailableDocksLabel.text = String(Stations.sharedInstance.list[indexPath.row].availableDocks)
+        cell.stationNameLabel.text = self.closestStations[indexPath.row].stationName
+        cell.numberOfBikesLabel.text = String(self.closestStations[indexPath.row].availableBikes)
+        cell.numberOfAvailableDocksLabel.text = String(self.closestStations[indexPath.row].availableDocks)
 
         return cell
     }
@@ -84,14 +80,5 @@ class StationsListTableViewController: UITableViewController, CLLocationManagerD
     
     func setupUI() {
         self.tableView.reloadData()
-    }
-    
-    func refreshStationsData() {
-        StationsDataService.sharedInstance.getAllStationData(SettingsService.sharedInstance.getSettingAsString(BIKE_SERVICE_API_URL_SETTINGS_KEY)) {
-            responseObject, error in
-            
-            Stations.sharedInstance.list = responseObject
-            self.tableView.reloadData()
-        }
     }
 }
