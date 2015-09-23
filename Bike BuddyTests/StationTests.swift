7b1a4c81e63278e3747107d36512ca42748f87e3
Tom Arra <tarra3@gmail.com>
2015-09-23 07:55:15 -0500

    Unit tests

--- Bike BuddyTests/StationTests.swift
+++ Bike BuddyTests/StationTests.swift
@@ -10,6 +10,12 @@ import UIKit
 import XCTest
 
 class StationTests: XCTestCase {
+    
+    private let testStationName = "Test Station Name"
+    private let testLatitude = 41.93250008
+    private let testLongitude = -87.65268082
+    private let testAvailableBikes = 3
+    private let testAvailableDocks = 3
 
     override func setUp() {
         super.setUp()
@@ -21,7 +27,37 @@ class StationTests: XCTestCase {
         super.tearDown()
     }
     
-    func testCreateObject() {
+    private func createBasicTestStation() -> Station {
+        let newStation = Station()
         
+        newStation.stationName = testStationName
+        newStation.latitude = testLatitude
+        newStation.longitude = testLongitude
+        newStation.availableBikes = testAvailableBikes
+        newStation.availableDocks = testAvailableDocks
+        
+        return newStation
+    }
+    
+    func testCreateBasicStation() {
+        let newStation = Station()
+        
+        XCTAssertNotNil(newStation, "Object should not be nil")
+    }
+    
+    func testCreateFullStation() {
+        let newStation = createBasicTestStation()
+        
+        XCTAssertEqual(newStation.stationName, testStationName, "Station name should be \(testStationName)")
+        XCTAssertEqual(newStation.latitude, testLatitude, "Station latitude should be \(testLatitude)")
+        XCTAssertEqual(newStation.longitude, testLongitude, "Station longitude should be \(testLongitude)")
+        XCTAssertEqual(newStation.availableBikes, testAvailableBikes, "Station available bikes should be \(testAvailableBikes)")
+        XCTAssertEqual(newStation.availableDocks, testAvailableDocks, "Station available docks should be \(testAvailableDocks)")
+    }
+    
+    func testStationTitleForMapAnnotation() {
+        let newStation = createBasicTestStation()
+        
+        XCTAssertEqual(newStation.title, testStationName, "Station title should match \(testStationName)")
     }
 }
