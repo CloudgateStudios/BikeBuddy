246625b1a5a9bdd66393b1b8478cefd6126dda94
Tom Arra <tarra3@gmail.com>
2015-09-23 16:51:58 -0500

    Trying to get snapshot to work well.

--- fastlane/snapshot.js
+++ fastlane/snapshot.js
@@ -5,4 +5,22 @@ var app = target.frontMostApp();
 var window = app.mainWindow();
 
 target.delay(3)
-captureLocalizedScreenshot("0-LandingScreen")
+target.frontMostApp().tabBar().buttons()["Stations List"].tap();
+captureLocalizedScreenshot("0-StationsList")
+
+target.delay(3)
+target.frontMostApp().tabBar().buttons()["Map"].tap();
+target.frontMostApp().mainWindow().elements()[1].elements()["Lake Shore Dr & North Blvd, Bikes: 5 Open Docks: 9"].tap();
+captureLocalizedScreenshot("1-Map")
+
+target.delay(3)
+target.frontMostApp().tabBar().buttons()["Settings"].tap();
+target.frontMostApp().mainWindow().tableViews()[0].cells()["City"].tap();
+captureLocalizedScreenshot("2-Cities")
+
+/*target.setDeviceOrientation(UIA_DEVICE_ORIENTATION_PORTRAIT);
+target.frontMostApp().tabBar().buttons()["Stations List"].tap();
+target.frontMostApp().tabBar().buttons()["Map"].tap();
+target.frontMostApp().mainWindow().elements()[1].elements()["Lake Shore Dr & North Blvd, Bikes: 5 Open Docks: 9"].tap();
+target.frontMostApp().tabBar().buttons()["Settings"].tap();
+target.frontMostApp().mainWindow().tableViews()[0].cells()["City"].tap();*/
--- readme.md
+++ readme.md
@@ -6,13 +6,15 @@
 A simple iOS app to get the nearest bike sharing stations to you. Works off of the fact that the company Bixi has a similar API for a lot of their bike sharing systems.
 
 ## Setup
-1. Install [Fastlane](https://fastlane.tools)
+1. Install xctool
+  * `sudo brew install xctool`
+2. Install [Fastlane](https://fastlane.tools)
   * `sudo gem install fastlane`
-2. Install [Carthage](https://github.com/Carthage/Carthage)
+3. Install [Carthage](https://github.com/Carthage/Carthage)
   * `sudo brew install carthage`
-3. Get the workspace ready
+4. Get the workspace ready
  * `fastlane setup`
-4. Open up the project file in Xcode 7
+5. Open up the project file in Xcode 7
 
 ## Third Party Use
 * [Alamofire](https://github.com/Alamofire/Alamofire)
