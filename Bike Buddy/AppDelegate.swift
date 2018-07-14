bb826ca7fa4767c5b93edf860e3cfd0e225c04df
Tom Arra <tarra3@gmail.com>
2018-07-14 07:26:49 -0500

    Trying to get launch from Siri Search to deeplink to Station Detail page, not working yet.

--- Bike Buddy/AppDelegate.swift
+++ Bike Buddy/AppDelegate.swift
@@ -14,19 +14,42 @@ import BikeBuddyKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // MARK: - Class Variables
+    
     var window: UIWindow?
     
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-        //SettingsService.sharedInstance
-        //AnalyticsService.sharedInstance
-        
-        ThemeService.applyTheme()
-        
-        Fabric.with([Answers.self, Crashlytics.self])
+    // MARK: - Basic App Launch Handlers
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
+        return true
     }
-
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
+        var returnValue = true
+        
+        setupApplication()
+        
+        /*if let activityDic = launchOptions?[UIApplicationLaunchOptionsKey.userActivityDictionary] as? [NSObject : AnyObject],
+         let activity = activityDic["UIApplicationLaunchOptionsUserActivityKey"] as? NSUserActivity {
+         handleActivity(activity)
+         }*/
+        
+        if let option = launchOptions {
+            if option.keys.contains(UIApplicationLaunchOptionsKey.userActivityDictionary) {
+                if let userActivityDict = option[UIApplicationLaunchOptionsKey.userActivityDictionary] as? [AnyHashable: Any] {
+                    if userActivityDict.keys.contains(UIApplicationLaunchOptionsKey.userActivityType) {
+                        if let userActivity = userActivityDict["UIApplicationLaunchOptionsUserActivityKey"] as? NSUserActivity {
+                            returnValue = handleActivity(userActivity: userActivity)
+                        }
+                    }
+                }
+            }
+        }
+        
+        return returnValue
+    }
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: Constants.NotificationCenterEvent.AppCameBackToForeground), object: self)
         
         if Stations.shouldBeUpdated() {
@@ -35,13 +58,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
     
+    // MARK: - User Activity Restore App Launch Handlers
+    
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        return handleActivity(userActivity: userActivity)
+    }
+    
+    func application(_ application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool {
+        if userActivityType == Constants.UserActivity.StationActivityTypeIdentifier {
+            return true
+        }
+        
+        return false
+    }
+    
+    // MARK: - Private Setup Functions
+    
+    private func setupApplication() {
+        ThemeService.applyTheme()
+        
+        Fabric.with([Answers.self, Crashlytics.self])
+    }
+    
+    private func handleActivity(userActivity: NSUserActivity) -> Bool {
         if userActivity.activityType == Constants.UserActivity.StationActivityTypeIdentifier {
             if self.window != nil {
                 //As long as we have the window (which is the tab bar controller at root) send the restore call down to it to handle
                 self.window?.rootViewController?.restoreUserActivityState(userActivity)
+                
+                return true
             }
         }
-        return true
+        
+        return false
     }
 }
