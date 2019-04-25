e121556dbba73f6c8ef65d293af764fe8000a83f
Tom Arra <tarra3@gmail.com>
2019-04-25 07:35:26 -0500

    Fixing issues from Hound

--- Bike Buddy/MapViewController.swift
+++ Bike Buddy/MapViewController.swift
@@ -150,9 +150,13 @@ extension MapViewController: MKMapViewDelegate {
             self.performSegue(withIdentifier: Constants.SegueNames.ShowStationDetailFromMap, sender: self)
         }
     }
-    
+
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-        //For some reason it seems like this delegate function is needed in order for the calloutAccessoryControlTapped function to actually be called. Seems like it's some kind of iOS bug.
+        /*
+        For some reason it seems like this delegate function is needed in order for
+        the calloutAccessoryControlTapped function to actually be called.
+        Seems like it's some kind of iOS bug.
+        */
     }
 
     /**
