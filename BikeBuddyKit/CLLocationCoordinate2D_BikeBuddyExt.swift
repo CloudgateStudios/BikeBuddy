2a636b0d31ab3246b6997f08fad76f1060b99055
Tom Arra <tarra3@gmail.com>
2026-02-13 11:44:06 -0600

    missed a file

--- BikeBuddyKit/CLLocationCoordinate2D_BikeBuddyExt.swift
+++ BikeBuddyKit/CLLocationCoordinate2D_BikeBuddyExt.swift
@@ -14,11 +14,6 @@ extension CLLocationCoordinate2D {
      Check to see if the location is actually a user location. If the user did not grant access to location then the coordinate that comes back is going to be 0.0 which will put them out in the middle of the ocean. Last time I checked that's not useful at all.
      */
     public func isCurrentUserLocationReal() -> Bool {
-        if self.latitude == 0.0 {
-            return false
-        } else {
-            return true
-        }
-
+        return self.latitude != 0.0 || self.longitude != 0.0
     }
 }
